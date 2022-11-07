67fcf20ea19fe88bed6dc44e5cd852cc4e7b78a7
Vladislav Skalozub <vladskalozub97@gmail.com>
2022-11-07 11:27:41 +0200

    Добавьте файлы проекта.

---
+++ Picture/Form1.cs
@@ -0,0 +1,15 @@
+namespace Picture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
